396c5acd1b05ad7ea7d443ae7e15883ef4f5b6f6
tkmn7486 <tkmn7486.pro@gmail.com>
2021-11-26 10:52:36 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <img alt="Vue logo" src="./assets/gear.png">
   <h1>Parts ID Copy</h1>
   <p>copyを押すと管理番号をコピーできます。</p>
-  <p>テスト</p>
+  <p class="test">テスト</p>
 </div>
 <div class="contents">
   <table id="table" border="1">
