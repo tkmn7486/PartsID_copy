17293c41ff56ae6518925a15929997115462447a
tkmn7486 <tkmn7486.pro@gmail.com>
2021-11-26 11:12:15 +0900

    third deploy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
 <div class="title">
-  <img alt="Vue logo" src="./assets/gear.png">
+  <img alt="Vue logo" src="./assets/logo.png" class="logo">
   <h1>Parts ID Copy</h1>
   <p>copyを押すと管理番号をコピーできます。</p>
-  <p class="test">テスト</p>
 </div>
-<div class="contents">
+<div class="water_proof">
+  <h3>防水テープ</h3>
   <table id="table" border="1">
-    <!-- テーブルヘッダー -->
     <thead>
       <tr>
         <th class="partsNumber">No.</th>
@@ -16,21 +15,39 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="part in parts_name" :key="part.id">
-        <th>{{part.number}}</th>
-        <th>{{part.name}}</th>
-        <th>{{part.id}}</th>
+      <tr v-for="wp_part in WP_list" :key="wp_part.id">
+        <th>{{wp_part.number}}</th>
+        <th>{{wp_part.name}}</th>
+        <th>{{wp_part.id}}</th>
         <td class="copyButton">
-          <button @click="copyID(part.id)" :key="button">COPY ID</button>
+          <button @click="copyID(wp_part.id)" :key="button">COPY ID</button>
         </td>
       </tr>
-      <!-- [1] ここに <tr> で ToDo の要素を1行づつ繰り返し表示したい -->
     </tbody>
   </table>
+</div>
 
-  <!-- <ul v-for="part in parts_name" :key="part.name">
-    <li>{{part.name}}</li><p class="wall"> | </p><li>{{part.id}}</li><button v-on:click='copyID'>copy</button>
-  </ul> -->
+<div class="glass_film">
+  <h3>保護フィルム</h3>
+  <table id="table" border="1">
+    <thead>
+      <tr>
+        <th class="partsNumber">No.</th>
+        <th class="partsName">Parts Name</th>
+        <th class="ID">Parts ID</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="gf_part in GF_list" :key="gf_part.id">
+        <th>{{gf_part.number}}</th>
+        <th>{{gf_part.name}}</th>
+        <th>{{gf_part.id}}</th>
+        <td class="copyButton">
+          <button @click="copyID(gf_part.id)" :key="button">COPY ID</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 </template>
 
@@ -50,10 +67,24 @@ export default {
     }
 
     return{
-      parts_name:[
+      WP_list:[
         {number:'01' ,name: 'iPhone 11', id:'SSI-FT-000001'},
         {number:'02' ,name: 'iPhone X', id:'SSI-FT-000002'},
         {number:'03' ,name: 'iPhone XR', id:'SSI-FT-000003'},
+        {number:'04' ,name: 'iPhone XS', id:'SSI-FT-000004'},
+        {number:'05' ,name: 'iPhone XS Max', id:'SSI-FT-000005'},
+        {number:'06' ,name: 'iPhone 7,8,SE2', id:'SSI-FT-000006'},
+        {number:'07' ,name: 'iPhone 8 Plus', id:'SSI-FT-000007'},
+      ],
+
+      GF_list:[
+        {number:'01' ,name: 'iPhone 11', id:'SSI-FT-000001'},
+        {number:'02' ,name: 'iPhone X', id:'SSI-FT-000002'},
+        {number:'03' ,name: 'iPhone XR', id:'SSI-FT-000003'},
+        {number:'04' ,name: 'iPhone XS', id:'SSI-FT-000004'},
+        {number:'05' ,name: 'iPhone XS Max', id:'SSI-FT-000005'},
+        {number:'06' ,name: 'iPhone 7,8,SE2', id:'SSI-FT-000006'},
+        {number:'07' ,name: 'iPhone 8 Plus', id:'SSI-FT-000007'},
       ],
       copyID,
     };
@@ -69,6 +100,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img.logo{
+  width:40px;
+  height:40px;
 }
 
 table{
